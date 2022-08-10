6d08f578ce2c40aee74fca297970b6cf63730c31
Kalinkina Elizaveta <kaliveta@gmail.com>
2022-08-10 14:11:17 +0800

    feat: add decides

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+	let str = number.toString().split("");
+	let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+	let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+
+	let result = "";
+	if (number < 20) {
+		result = a[number];
+	}
+	if (number >= 20 && number < 100) {
+		result = (str[1] == 0) ? b[number / 10] : `${b[Math.floor(number / 10)]} ${a[number % 10]}`;
+	}
+
+	if (str.length === 3) {
+		if (str[1] == 0 && str[2] == 0) {
+			result = `${a[Math.floor(number / 100)]} hundred`;
+		}
+		else if (str[1] == 0 && str[2] != 0) {
+			result = `${a[Math.floor(number / 100)]} hundred ${a[number % 100]}`;
+		} else if
+			(str[1] == 1) { result = `${a[Math.floor(number / 100)]} hundred ${a[number % 100]}`; }
+		else if (str[1] != 0 && str[2] == 0) {
+			result = `${a[Math.floor(number / 100)]} hundred ${b[(number % 100) / 10]}`;
+		}
+		else {
+			result = `${a[Math.floor(number / 100)]} hundred ${b[Math.floor((number % 100) / 10)]} ${a[number % 10]}`;
+		}
+	}
+	return result;
+
+};
+
